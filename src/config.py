35fc4e250b4bf76b6a5912c61c93923836885d26
Vlad Mashtalier <master@Vlads-MacBook-Air.local>
2025-10-24 19:02:32 +0300

    TARGET removed from COLS_TO_USE

--- src/config.py
+++ src/config.py
@@ -6,7 +6,7 @@ LOG_SCALE_COLS = ["1stFlrSF", "GrLivArea"]
 
 STANDARD_SCALE_COLS = ["YearBuilt", "YearRemodAdd", "LowQualFinSF",  "2ndFlrSF", "1stFlrSF", "GrLivArea"]
 
-COLS_TO_USE = ["YearBuilt", "YearRemodAdd", "1stFlrSF", "2ndFlrSF", "LowQualFinSF", "GrLivArea", TARGET]
+COLS_TO_USE = ["YearBuilt", "YearRemodAdd", "1stFlrSF", "2ndFlrSF", "LowQualFinSF", "GrLivArea"]
 
 NUMERICAL_COLS_TO_FILL = []
 
